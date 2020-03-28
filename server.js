f5dbfa071f39f5f0526f88c0a586d3b4ecd2864d
EMC <edwardcoad.ec@gmail.com>
2020-03-28 13:38:01 +1030

    fixes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and views.
+// Add routes, both API and view.
 app.use(routes);
 
 // Connect to the Mongo DB
