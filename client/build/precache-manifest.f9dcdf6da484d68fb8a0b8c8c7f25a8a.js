db54f6e03b308502f84e82b396f0ef613ead5aa6
EMC <edwardcoad.ec@gmail.com>
2020-03-28 13:21:44 +1030

    fix

---
+++ client/build/precache-manifest.f9dcdf6da484d68fb8a0b8c8c7f25a8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f43898c440b259c6f5b4",
+    "url": "/static/css/main.53e6c944.chunk.css"
+  },
+  {
+    "revision": "f43898c440b259c6f5b4",
+    "url": "/static/js/main.1f98585f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "6fdfc5c8c562ff69614ea44a8056f4a0",
+    "url": "/index.html"
+  }
+];
